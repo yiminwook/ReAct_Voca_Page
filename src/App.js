c39ed0837fd4579210fcadf01e3b90130ec3afb8
yiminwook <grs0416@gmail.com>
2022-08-01 18:31:08 +0900

    initiate: Voca Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Hello from './component/Hello.js';
+import Welcome from './component/Welcome.js';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello />
+      <Hello />
+      <Hello />
+      <Welcome />
     </div>
   );
 }
