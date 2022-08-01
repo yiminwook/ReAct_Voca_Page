06395268f2a97b5db4e639b5e5f9356ccbb2870d
yiminwook <grs0416@gmail.com>
2022-08-01 20:27:16 +0900

    create component_files

---
+++ src/component/DayList.js
@@ -0,0 +1,14 @@
+import dummy from '../db/data.json'
+
+const DayList = () => {
+  console.log(dummy)
+  return ( 
+  <ul className='list_day'>
+    {dummy.days.map(day=>(
+      <li key={day.id}>Day {day.day}</li>
+    ))}
+  </ul>
+  );
+}
+
+export default DayList;
---
+++ src/component/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+  <div className='header'>
+    <h1>
+      <a>토익 영단어(고급)</a>
+    </h1>
+    <div className="menu">
+    <a herf='' className="link">
+        단어추가
+    </a>
+    <a herf='' className="link">
+        Day 추가
+    </a>
+    </div>
+  </div>
+  );
+}
+
+export default Header;
